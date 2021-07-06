dab15f9b3b256cc9ae737413b019099d2d635fc2
Sagar <acharyasaagar@gmail.com>
2021-07-06 11:56:25 +0300

    Adds /version endpoint

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import packageJson from './package.json'
 
 const app = express()
 
@@ -6,6 +7,10 @@ const PORT = process.env.PORT || 5555
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/version', (req, res) => {
+  res.send(packageJson.version)
 })
 
 app.listen(PORT, () => console.log('Server is listening at port', PORT))
